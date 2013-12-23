7157e3091ef63f0ca7cf1d67af3b1563cdc91ab4
Petar <petar.ivacic@temple.edu>
2013-12-23 00:12:26 -0500

    Chat is working

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
-var express = require('express'), app = express.createServer();
-var logfmt = require('logfmt');
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
-app.use(logfmt.requestLogger());
+server.listen(5000);
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+// Routing
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function(){
-  console.log("Listening on " + port);
+// Usernames which are currently connected to the chat
+var usernames = {};
+
+io.sockets.on('connection', function (socket) {
+  //when the client emits 'sendmessage'
+  socket.on('sendmessage', function(data){
+    io.sockets.emit('updatemessage', socket.username, data);
+  });
+
+  //Add User
+  socket.on('adduser', function(username){
+    socket.username = username;
+    usernames[username] = username;
+    socket.emit('updatechat', 'Petar Ivacic', 'you are connected');
+    socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+    io.sockets.emit('updateusers', usernames);
+  });
+
+socket.on('disconnect', function(){
+  delete usernames[socket.username];
+  io.sockets.emit('updateusers', usernames);
+  socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
 });
+});
+
 
 
